2c96b5aa020aadcd8c0031fec27dfa39c0d7431f
Bruno Wünsch <bw004862@cfjl.com.br>
2022-11-14 19:57:56 -0300

    Criado método que renderiza em memória arquivo php

--- src/View/Render.php
+++ src/View/Render.php
@@ -37,5 +37,35 @@ class Render
         require_once $pathPagina;
         require_once TFRONTEND . 'common/bottom.php';
     }
+
+    /**
+     * Método que retorna o conteúdo de um bloco (arquivo.php)
+     *
+     * @param string $bloco Nome do bloco a ser renderizado e retornado
+     * @param array $dados Dados a serem inseridos o bloco
+     * @return void
+     */
+    static public function block(string $bloco, array $dados = [])
+    {
+        // Monta o caminho do local onde o bloco solicitada está
+        $pathArquivo = TFRONTEND . 'blocks/' . $bloco . '.php';
+
+        if ( !file_exists($pathArquivo) ) {
+            error_log('Bloco não localizado em:' . $pathArquivo);
+            throw new Exception("O bloco solicitado'{$bloco}'não foi localizado");
+        }
+
+        // Transforma os índices do vetor em variáveis
+        extract($dados);
     
+        // Inicia a captura do buffer para não printar ao
+        // usuario o conteúdo do arquivo que será requerido
+        ob_start();
+
+        // Carrega o conteúdo do arquivo para o buffer (estamos em OB_START)
+        require_once $pathArquivo;
+
+        // Retorna o conteúdo em buffer e limpa a memória
+        return ob_get_clean();
+    }
 }
