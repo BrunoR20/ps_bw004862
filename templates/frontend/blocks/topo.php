71bfc11b3832de041098655a275c3e36de297b62
Bruno Wünsch <bw004862@cfjl.com.br>
2022-11-14 23:48:31 -0300

    Criado o topo padrão das páginas

--- templates/frontend/blocks/topo.php
+++ templates/frontend/blocks/topo.php
@@ -1,1 +1,87 @@
-Conteúdo do topo
+<nav class="navbar navbar-expand-sm navbar-dark bg-black" aria-label="Third navbar example">
+    <div class="container-fluid d-flex pt-2">
+
+        <div class="d-flex flex-column w-10">
+            <a class="navbar-brand text-center border border-1 rounded mb-2 me-0" href="#">LOGO</a>
+            <div class="d-inline-flex">
+                <span class="nav-item dropdown text-white">
+                    <a class="nav-link dropdown-toggle text-white mt-2 mb-sm-0" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi bi-list"></i>
+                        Departamentos
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Categoria A</a></li>
+                        <li><a class="dropdown-item" href="#">Categoria B</a></li>
+                        <li><a class="dropdown-item" href="#">Categoria C</a></li>
+                        <li><a class="dropdown-item" href="#">Categoria D</a></li>
+                        <li><a class="dropdown-item" href="#">Categoria E</a></li>
+                    </ul>
+                </span>
+            </div>
+        </div>
+        
+        <div class="d-flex flex-column w-50">
+            <div class="d-inline-flex input-group w-auto">
+                <div class="input-group mb-2">
+                    <input type="text" class="form-control" placeholder="Procurar" aria-label="Procurar" aria-describedby="basic-addon2">
+                    <a class="input-group-text text-black" id="basic-addon2" href="#">
+                        <i class="bi bi-search"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="d-inline-flex">
+                <div class="collapse navbar-collapse" id="navbarsExample03">
+                    <ul class="navbar-nav my-2 mb-sm-0 font-size-sm">
+                        <li class="nav-item border border-1 rounded me-2">
+                            <a class="nav-link text-white" href="#">Nossas lojas</a>
+                        </li>
+                        <li class="nav-item border border-1 rounded">
+                            <a class="nav-link text-white" href="#">Fale conosco</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex flex-column w-40">
+            <div class="d-inline-flex">
+                <div class="collapse navbar-collapse" id="navbarsExample03">
+                        <ul class="navbar-nav me-auto mb-2">
+                            <li class="nav-item border border-1 rounded me-2">
+                                <a class="nav-link text-white" href="#">
+                                    <i class="bi bi-person"></i>
+                                    Login/cadastro
+                                </a>
+                            </li>
+                            <li class="nav-item border border-1 rounded me-2">
+                                <a class="nav-link text-white" href="#">
+                                    <i class="bi bi-heart"></i>
+                                    Favoritos
+                                </a>
+                            </li>
+                            <li class="nav-item border border-1 rounded">
+                                <a class="nav-link text-white" href="#">
+                                    <i class="bi bi-cart2"></i>    
+                                    Carrinho
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            <div class="d-inline-flex">
+                <div class="collapse navbar-collapse justify-content-end" id="navbarsExample03">
+                        <ul class="navbar-nav mb-2 mt-2 mb-sm-0 font-size-sm">
+                            <li class="nav-item border border-1 rounded">
+                                <a class="nav-link text-white" href="#">Fone: 55 5555-1111</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
