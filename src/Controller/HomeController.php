9ab917e14e97b04ca95865b76c4af30d61764dc2
Bruno Wünsch <bw004862@cfjl.com.br>
2022-11-18 20:07:29 -0300

    Criada classe abstrata p/ renderizar topo e rodapé

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,22 @@
 
 namespace Petshop\Controller;
 
-class HomeController
+use Petshop\Core\FrontController;
+use Petshop\Model\Estado;
+use Petshop\View\Render;
+
+class HomeController extends FrontController
 {
     public function index()
     {
-        echo 'aaaa';
+        $estados = (new Estado())->find();
+
+        $dados = [];
+        $dados['titulo'] = 'Lista de Estados';
+        $dados['estados'] = $estados;
+        $dados['topo'] = $this->carregaHTMLTopo();
+        $dados['rodape'] = $this->carregaHTMLRodape();
+        
+        Render::front('home', $dados);
     }
 }
