0ce1cce66da8a5b3ae738b27d423ed7d10c0f067
Bruno Wünsch <bw004862@cfjl.com.br>
2022-11-21 21:53:49 -0300

    Rota de POST para /cadastro e abertura de sessão

--- src/Core/App.php
+++ src/Core/App.php
@@ -24,6 +24,9 @@ class App
      */
     public static function start()
     {
+        // Carrega uma sessão ou inicia uma nova em caso de novo acesso
+        self::carregaSessao();
+
         // Associa um objeto Bramus\Router à variável $router
         self::$router = new Router();
         
@@ -47,6 +50,7 @@ class App
         self::$router->get('/','\Petshop\Controller\HomeController@index');
         self::$router->get('/login','\Petshop\Controller\LoginController@login');
         self::$router->get('/cadastro','\Petshop\Controller\CadastroController@cadastro');
+        self::$router->post('/cadastro','\Petshop\Controller\CadastroController@postCadastro');
     }
 
     /**
@@ -72,5 +76,16 @@ class App
             $objErro = new ErrorController();
             $objErro->erro404();
         });
+    }
+
+    /**
+     * Função que inicia uma nova versão e, posteriormente,
+     * carrega o ID da sessão gravada no dispositivo do usuário
+     *
+     * @return void
+     */
+    private static function carregaSessao()
+    {
+        session_start();
     }
 }
