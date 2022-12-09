5a31bf1235e435f7b26761c8790c09b73f52b160
Bruno Wünsch <bw004862@cfjl.com.br>
2022-12-09 19:50:06 -0300

    Alteração ao linkar sweet alert

--- templates/frontend/common/bottom.php
+++ templates/frontend/common/bottom.php
@@ -7,6 +7,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js" integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- SCRIPTS DO PROJETO -->
-    <script src="/assets/js/scipt.js"></script>
+    <script src="/assets/js/script.js"></script>
 </body>
 </html>
