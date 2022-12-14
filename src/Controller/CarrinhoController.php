f579f5753f041eeb7bcae7922dd60c040964fb9e
Bruno Wünsch <bw004862@cfjl.com.br>
2022-12-13 21:39:40 -0300

    Criado controller para listagem de prod. carrinho

---
+++ src/Controller/CarrinhoController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Petshop\Controller;
+
+use Petshop\Core\DB;
+use Petshop\Core\FrontController;
+use Petshop\Model\Produto;
+use Petshop\View\Render;
+
+class CarrinhoController extends FrontController
+{
+    public function listar()
+    {
+        $dados = [];
+        $dados['topo'] = $this->carregaHTMLTopo();
+        $dados['rodape'] = $this->carregaHTMLRodape();
+
+        $sql = 'SELECT p.idproduto, p.nome, p.preco, cp.quantidade
+                FROM carrinhos c
+                INNER JOIN carrinhosprodutos cp ON cp.idcarrinho = c.idcarrinho
+                INNER JOIN produtos p ON p.idproduto = cp.idproduto
+                WHERE c.idcliente = ?
+                ORDER BY cp.created_at DESC';
+
+        $idCliente = $_SESSION['cliente']['idcliente'] ?? 0;
+        $rowsProdutos = DB::select($sql, [$idCliente]);
+
+        $produto = new Produto();
+
+        foreach ($rowsProdutos as &$p) {
+            $produto->loadById($p['idproduto']);
+
+            $p['imagens'] = $produto->getFiles();
+            $p['desconto'] ??= 0.15;
+            $p['precodesconto'] = $p['preco'] * (1 -  $p['desconto']);
+        }
+        
+        $dados['produtos'] = $rowsProdutos;
+        
+        Render::front('carrinho', $dados);
+    }
+}
