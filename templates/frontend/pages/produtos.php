b18042f12cf0d76217a43a460be6c5871cb88207
Bruno Rafael Wünsch <bruno_wunsch@hotmail.com>
2022-12-18 16:31:16 -0300

    Alteração no design da página

--- templates/frontend/pages/produtos.php
+++ templates/frontend/pages/produtos.php
@@ -1,69 +1,110 @@
 <div class="container mt-3 pg-produtos">
-    <div class="row">
+
+    <div class="row mx-auto">
+
         <div class="col-8 row p-3">
             <div class="col-5">
                 <img class="img-fluid img-produto" src="<?= $imagens[0]['url'] ?>" alt="<?= $produto['nome'] ?>">
             </div>
+            
             <div class="col-7">
                 <h1 class="h3 text-justify"><?= $produto['nome'] ?></h1>
-                <p><?= str_replace("\n", '<br>', $produto['descricao']) ?></p>
+                <p class="mt-3"><?= str_replace("\n", '<br>', $produto['descricao']) ?></p>
             </div>
         </div>
 
         <div class="col-3 ms-auto bg-light rounded p-3">
-            <div class="preco preco-desconto">
-                <span class="text-decoration-line-through text-muted">
-                    R$ <?= number_format($produto['preco'] + $produto['preco'] * 0.15, 2, ',', '.') ?>
-                    <span class="ms-2 badge rounded-pill text-bg-success">
-                        <?= $produto['desconto'] * 100 ?>% off
+
+            <div class="row align-items-top d-flex justify-content-between align-items-center">
+
+                <div class="col-10">
+                    <span class="text-decoration-line-through text-muted">
+                        R$<?= number_format($produto['preco'] + $produto['preco'] * 0.15, 2, ',', '.') ?>
                     </span>
-                </span>
+
+                    <span class="ms-2 badge rounded-pill text-bg-success">
+                        <?= $produto['desconto'] * 100 ?>% OFF
+                    </span>
+                </div>
+
+                <div class="col-2">
+                    <a href="#"
+                       class="fs-4 text-danger curtir-produto"
+                       data-idproduto="<?= $produto['idproduto'] ?>"
+                       title="Favoritar este produto">
+                        
+                        <i class="bi <?= ($produto['ativo'] == 'S') ? 'bi-heart-fill' : 'bi-heart' ?>"></i>
+                    </a>
+                </div>
+                
             </div>
 
-            <div class="preco preco-full">
-                <span class="fs-1">
-                    R$ <?= number_format($produto['preco'], 2, ',', '.') ?>
-                </span>
+            <div class="fs-2 fw-semibold">
+                R$<?= number_format($produto['preco'], 2, ',', '.') ?>
             </div>
-
-            <div class="fw-bold">á vista o cartão, pix ou boleto</div>
 
             <?php
                 if ($produto['precodesconto'] > 80):  
             ?>
-            <div class="text-muted">
-                <i class="bi bi-credit-card"></i>
-                no cartão em até 8x de R$
-                <?= number_format($produto['preco']/8, 2, ',', '.') ?> <br>
-                sem juros
-            </div>
+                <div class="fs-5 mb-1">
+                    <span>em</span>
+
+                    <span class="text-success fw-semibold">
+                        8x R$<?= number_format($produto['preco']/8, 2, ',', '.') ?> sem juros
+                    </span>
+                </div>
             <?php
                 endif;
             ?>
 
-            <div class="btn-adicionar mt-4 row">
-                <div class="col-3 me-auto text-center">
-                    <a href="#"
-                       class="fs-4 text-danger p-2 curtir-produto"
-                       data-idproduto="<?= $produto['idproduto'] ?>"
-                       title="Favoritar este produto">
-                       
-                        <i class="bi <?= ($produto['ativo'] == 'S') ? 'bi-heart-fill' : 'bi-heart' ?>"></i>
-                    </a>
-                </div>
-                <div class="col-8">
-                    <a href="#"
-                       class="btn btn-danger w-100 comprar-produto"
-                       data-idproduto="<?= $produto['idproduto'] ?>"
-                       data-quantidade="1">
+            <a href="#" class="text-decoration-none">Ver os meios de pagamento</a>
 
-                        <i class="bi bi-cart-check"></i> Comprar
-                    </a>
+            <div class="mt-3">
+                <div class="text-success fw-semibold">
+                    <i class="bi bi-truck"></i> Frete grátis 
                 </div>
             </div>
 
-        </div>
+            <div class="mt-3">
+                <div class="h6 fs-5">
+                    Estoque disponível
+                </div>
 
+                <div class="mt-1">
+                    <span>
+                        Quantidade:
+                    </span>
+
+                    <span class="fw-bold">
+                        <?= $produto['quantidade'] ?> unidades
+                    </span>
+                </div>
+            </div>
+
+            <div class="mt-3 row">
+
+                <div class="col-12">
+                    <a href="#"
+                       class="btn btn-primary w-100 border-0 py-2 px-1 fw-semibold"
+                       data-idproduto="<?= $produto['idproduto'] ?>"
+                       data-quantidade="1">
+
+                        Comprar agora
+                    </a>
+                </div>
+
+                <div class="col-12 mt-2">
+                    <a href="#"
+                       class="btn btn-secondary w-100 border-0 py-2 px-1 fw-semibold text-primary bg-secondary bg-opacity-25 comprar-produto
+                       data-idproduto="<?= $produto['idproduto'] ?>"
+                       data-quantidade="1">
+
+                        Adicionar ao carrinho
+                    </a>
+                </div>
+                
+            </div>
+        </div>
     </div>
 
     <div class="row mt-5">
